47cd9a0f437ce0924bc3addc0a95dd891910f841
Saiful Islam <https://github.com/Ferrari2003>
2022-09-26 05:24:29 +0600

    add to commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import "./Cart.css";
+const Cart = (props) => {
 
-const Cart = ({cart}) => {
+       const {cart}  =props;
+
+             
+
+          let total = 0;
+          let shipping =0;
+          for(const product of cart){
+            total = total + product.price;
+            shipping =shipping + product.shipping;
+          }
+         const tax = (total * 0.1).toFixed(2);
+             
+
+
     return (
-        <div>
+        <div className='cart'>
              <h3>Order Summary</h3>
                 <h4>Selected Items: {cart.length}</h4>
+                <p>Total price: ${total}</p>
+                <p>Total Shipping: ${shipping} </p>
+                <p>Tax: {tax}</p>
+                <h5>Grand Total: </h5>
         </div>
     );
 };
