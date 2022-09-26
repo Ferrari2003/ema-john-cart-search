5e23c763a1310d7817c30ee24622c9dce3436811
Saiful Islam <https://github.com/Ferrari2003>
2022-09-26 07:56:37 +0600

    add commit

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -8,16 +9,37 @@ const Shop = () => {
     const [cart, setCart] = useState([]);
 
     useEffect( () =>{
+        console.log('load product')
         fetch('products.json')
         .then(res=> res.json())
         .then(data => setProducts(data))
     }, []);
+
+    useEffect(() => {
+        console.log('local storage')
+        const storedCart =getStoredCart();
+       const saveCart =[];
+        for(const id in storedCart){
+       const addedProduct =products.find(product => product.id ===id)
+       if(addedProduct){
+            const quantity = storedCart[id];
+            addedProduct.quantity= quantity;
+              console.log(addedProduct);
+              saveCart.push(addedProduct);
+       }
+        }
+        setCart(saveCart);
+        // console.log('finished')
+    },[products])
+
+
 
     const handleAddToCart = (product) =>{
         console.log(product);
         // do not do this: cart.push(product);
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id);
     }
 
     return (
